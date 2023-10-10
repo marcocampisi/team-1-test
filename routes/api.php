60b8d55854a9f05dd4f731eae6944fb34543d3b8
marcocampisi <marco.campisi@outlook.it>
2023-10-10 12:50:52 +0200

    Add ProductResource

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\Orders\OrderController;
+use App\Http\Controllers\Api\Products\ProductController;
+use App\Models\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('products', [ProductController::class, 'index']);
 Route::get('orders/generate', [OrderController::class, 'generate']);
 Route::post('orders/make/payment', [OrderController::class, 'makePayment']);
