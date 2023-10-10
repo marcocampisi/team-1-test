5027aea5c1e6f012b7f4e4708dab91800855135e
marcocampisi <marco.campisi@outlook.it>
2023-10-10 12:13:36 +0200

    Response from api

--- app/Http/Controllers/Api/Orders/OrderController.php
+++ app/Http/Controllers/Api/Orders/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Orders;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Orders\OrderRequest;
 use Illuminate\Http\Request;
 use Braintree\Gateway;
 
@@ -20,9 +21,29 @@ class OrderController extends Controller
         return response()->json($data, 200);
     }
 
-    public function makePayment(Request $request, Gateway $gateway) {
+    public function makePayment(OrderRequest $request, Gateway $gateway) {
 
-        
+        $result = $gateway->transaction()->sale([
+            'amount' => '10.00',
+            'paymentMethodNonce' => $request->token,
+            'options' => [
+                'submitForSettlement' => true
+            ]
+        ]);
+
+        if($result->success) {
+            $data = [
+                'success' => true,
+                'message' => 'Transazione eseguita'
+            ];
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'success' => true,
+                'message' => 'Transazione fallita'
+            ];
+            return response()->json($data, 401);
+        }
 
         return 'make payment';
     }
